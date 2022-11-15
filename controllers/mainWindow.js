f448984bd8da3ef44be54efd10fae3b3f0054e64
sschrs <s_ozarslan07@outlook.com>
2022-11-15 16:31:10 +0300

    draw rectangle for selected area

--- controllers/mainWindow.js
+++ controllers/mainWindow.js
@@ -1,5 +1,5 @@
 $(window).on("load",()=>{
-
+    var selected_points = []
     var ctx = document.getElementById("certificate-canvas").getContext("2d")
     var img = new Image;
     img.onload = ()=>{
@@ -16,14 +16,30 @@ $(window).on("load",()=>{
     var upload_link = document.getElementById("upload-certificate-link")
     upload_link.addEventListener("click", fileUpload)
 
+    var certificate_canvas = document.getElementById("certificate-canvas")
+
     ipcRenderer.on("selected", function(err,data){
         upload_link.removeEventListener("click", fileUpload)
         img.src = data
-        document.getElementById("certificate-canvas").addEventListener("click",function(e){
-            alert(e.pageX - 515)
-            alert(e.pageY - 10)
+        certificate_canvas.addEventListener("click",function(e){
+            if (selected_points.length < 2){
+                selected_points.push({
+                    "x" : e.pageX - 515,
+                    "y" : e.pageY - 10
+                })
+
+                if (selected_points.length == 2){
+                    x = selected_points[0].x
+                    y = selected_points[0].y
+                    width = selected_points[1].x - x
+                    height = selected_points[1].y - y
+                    ctx.beginPath()
+                    ctx.rect(x, y, width, height)
+                    ctx.stroke()
+                }
+            }
         })
     })
-    
 })
+
 
